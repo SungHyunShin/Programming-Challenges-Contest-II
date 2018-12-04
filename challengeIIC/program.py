0e6aa879d753897cd706a77b128ca17a821b7ca0
SungHyun Shin <sshin1@student04.cse.nd.edu>
2018-12-04 17:35:21 -0500

    c done

--- challengeIIC/program.py
+++ challengeIIC/program.py
@@ -3,9 +3,21 @@
 
 import sys
 
+def longestPerm(word1,word2):
+	perm = []
+	word2 = [x for x in word2]
+	for char in word1:
+		if char in word2:
+			word2.remove(char)
+			perm += char
+	if perm:
+		perm.sort()
+		print("".join(perm))
+	else:
+		print()
+
 #main
 if __name__ == '__main__':
 	lines = [line.rstrip() for line in sys.stdin.readlines()]
 	for i in range(0,len(lines),2):
-		print(lines[i])
-		print(lines[i+1])
+		longestPerm(lines[i],lines[i+1])
