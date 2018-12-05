b05acbf0274de3d7a17a3e886630d6e3fd1eecc4
SungHyun Shin <sshin1@student04.cse.nd.edu>
2018-12-05 18:47:28 -0500

    D done

--- challengeIID/program.py
+++ challengeIID/program.py
@@ -2,73 +2,77 @@
 # Sung Hyun Shin (sshin1) & Luke Song (csong1)
 
 import sys
-import collections
-import difflib
-import copy
 
+# checks if two words are word morphs of each other
 def isWordMorph(w1,w2):
 	diff = 0
-	for s in difflib.ndiff(w1,w2):
-		if s[0] != ' ':
+	# if w2 is 1 char longer
+	if len(w1)+1 == len(w2):
+		if w1[0] is not w2[0]:
+			w2 = w2[1:]
 			diff += 1
-	return diff == 2 and len(w1) == len(w2) or diff == 1 and len(w1) == len(w2)+1 or diff == 1 and len(w1)+1 == len(w2)
+		elif w1[-1] is not w2[-1]:
+			w2 = w2[:-1]
+			diff += 1
+	# else if w1 is 1 char longer
+	elif len(w1) == len(w2)+1:
+		if w1[0] is not w2[0]:
+			w1 = w1[1:]
+			diff += 1
+		elif w1[-1] is not w2[-1]:
+			w1 = w1[:-1]
+			diff += 1
+	# else if they're not the same length
+	elif len(w1) is not len(w2):
+		return False
+	# if they're equal lengths or 1 char longer than each other
+	if len(w1) == len(w2):
+		for i in range(len(w1)):
+			if w1[i] is not w2[i]:
+				diff +=1
+		# if there's one or less differences
+		return diff <= 1
+	# else return false
+	else:
+		return False
 
-
+# edge list where if 1 can go to 2 or 3, edgeL[1] = [2,3]
 def buildedgeL(wordL):
 	edgeL = []
 	for i in range(len(wordL)):
-		edgeL.append([0]*len(wordL))
+		edgeL.append([])
 	for i in range(len(wordL)):
 		for j in range(i+1, len(wordL)):
 			if isWordMorph(wordL[j],wordL[i]):
-				edgeL[i][j] = 1
+				edgeL[i].append(j)
 	return edgeL
 
-def bfs(edgeL,v, end):
-
-	
-	frontier = []
-	path = []
-	path.append(v)
-	frontier.append((v,path))
-	all_paths = []
-	while frontier:
-		x = min(frontier,key=lambda t: len(t[1]))
-		frontier.remove(x)
-		v, path = x
-		
-		if v == end:
-			all_paths.append(path)
-			continue
-		for i,u in enumerate(edgeL[v]):
-			if u == 1:
-				p = copy.deepcopy(path)
-				p.append(i)
-				frontier.append((i,p))
-	return all_paths
+# recursive depth first that returns a list of all paths
+def dfs_rec(edgeL,v,marked,path,paths):
+	if v in marked:
+		return
+	marked.add(v)
+	paths.append(path)
+	for i in edgeL[v]:
+		p = path.copy()
+		p.append(i)
+		dfs_rec(edgeL,i,marked,p,paths)
+	return paths
 
 #main
 if __name__ == '__main__':
 	wordL = [line.rstrip() for line in sys.stdin.readlines()]
 	wordL.sort()
-	#print(wordL)
 	edgeL = buildedgeL(wordL)
-
-	longestP = []
-	paths = []
-	for v in range(len(wordL)):
-		for k in range(v, len(wordL)):
-			curr = bfs(edgeL,v, k)
-			if curr != []:
-				for path in curr:
-					paths.append(path)
-	longest = 0
-	final_path = []
-	for path in paths:
-		if len(path) > longest:
-			longest = len(path)
-			final_path = path
-	print(longest)
-	for i in sorted(final_path):
+	longestPath = []
+	# run dfs for all nodes (bc they can be disconnected) and get longest path
+	for edge in range(len(edgeL)):
+		path = []
+		path.append(edge)
+		paths = dfs_rec(edgeL,edge,set(),path,[])
+		longestPath = max(longestPath,max(paths,key=len),key=len)
+	# print cost
+	print(len(longestPath))
+	# print words
+	for i in longestPath:
 		print(wordL[i])
-		
